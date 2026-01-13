e4ffb5e991b357f90235c5b5535b39d94b451231
VacuumHui <ragnarok16801050@gmail.com>
2026-01-13 15:43:59 +0300

    Update GameState.js

--- src/GameState.js
+++ src/GameState.js
@@ -1,12 +1,14 @@
 // src/GameState.js
 
 export const GameState = {
-    // Стартовая колода: 3 Удара, 3 Обороны
-    deck: ["strike", "strike", "strike", "defend", "defend", "defend"], 
-    
+    // Стартовая колода: 2 Удара, 3 Обороны
+    deck: ["strike", "strike", "defend", "defend", "defend"], 
+
+    relics: [], // <-- СПИСОК ID РЕЛИКВИЙ (например, ["strawberry", "dumbbell"])
     // Здоровье героя (чтобы сохранялось между боями)
     maxHp: 50,
     currentHp: 50,
+    gold: 0,    // <-- ВАЛЮТА
     
     // Текущий уровень (можно использовать для усиления врагов)
     level: 1
