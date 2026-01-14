047dd9f995572b530d959e5b9dcff94b1f43d226
VacuumHui <ragnarok16801050@gmail.com>
2026-01-14 12:46:44 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
 // src/main.js
 import { BattleScene } from './scenes/BattleScene.js';
-import { MapScene } from './scenes/MapScene.js';
+import { MapScene } from './scenes/MapScene.js'; // <--- ВАЖНО: Импорт новой сцены
 
 const config = {
     type: Phaser.AUTO,
     scale: {
-        // FIT растягивает игру, сохраняя пропорции
-        mode: Phaser.Scale.FIT, 
-        // Центрируем игру на экране
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        
-        width: 1600, 
+        width: 1600,
         height: 720
     },
-    // Делаем фон черным, чтобы сливался с рамками на очень длинных телефонах
-    backgroundColor: '#000000', 
+    backgroundColor: '#000000',
     pixelArt: true,
-    scene: [MapScene,BattleScene]
+    // ВАЖНО: MapScene должна быть ПЕРВОЙ в списке!
+    // Первая сцена в массиве запускается автоматически при старте.
+    scene: [MapScene, BattleScene] 
 };
 
 const game = new Phaser.Game(config);
