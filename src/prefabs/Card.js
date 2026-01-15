4ef1984025e5b0cc3da7639a4d04d87392acb1f7
VacuumHui <ragnarok16801050@gmail.com>
2026-01-15 16:31:12 +0300

    Update Card.js

--- src/prefabs/Card.js
+++ src/prefabs/Card.js
@@ -1,7 +1,6 @@
 // Файл: src/prefabs/Card.js
 
 import { CARDS_DB } from '../data/cards.js';
-import { ENCHANTS_DB } from '../data/enchants.js'; // <--- НОВЫЙ ИМПОРТ
 import { getComputedCard } from '../managers/CardLogic.js';
 
 export class Card extends Phaser.GameObjects.Container {
@@ -10,46 +9,49 @@ export class Card extends Phaser.GameObjects.Container {
         this.scene = scene;
         this.cardInstance = cardInstance; 
         
-        // Получаем финальные цифры
-        this.cardData = getComputedCard(cardInstance);
+        // 1. ПОЛУЧАЕМ ВСЕ ДАННЫЕ ИЗ ЛОГИКИ
+        this.computedData = getComputedCard(cardInstance);
         const baseData = CARDS_DB[cardInstance.id];
         
         this.isZoomed = false;
         
         const w = 100, h = 140;
+        
+        // Цвет рамки (редкость + зачарование)
         let strokeColor = 0x999999;
         if (baseData.rarity === 'rare') strokeColor = 0x0088ff;
         if (baseData.rarity === 'legendary') strokeColor = 0xffaa00;
         
-        // --- ФОРМИРОВАНИЕ ОПИСАНИЯ ---
-        let descText = this.cardData.desc;
-        
-        // Если есть зачарования, добавляем их описание
         if (cardInstance.enchants && cardInstance.enchants.length > 0) {
-            strokeColor = 0xff00ff; // Фиолетовая рамка
-            
-            cardInstance.enchants.forEach(enchantId => {
-                const enchant = ENCHANTS_DB[enchantId];
-                if (enchant) {
-                    // Добавляем текст: "Руна Огня: +2 Яда"
-                    descText += `\n[${enchant.desc}]`;
-                }
-            });
+            strokeColor = 0xff00ff; // Фиолетовый, если есть чары
         }
-        // ------------------------------
 
         this.bg = scene.add.rectangle(0, 0, w, h, 0x222222).setStrokeStyle(2, strokeColor);
         this.art = scene.add.rectangle(0, -30, 80, 60, baseData.color);
         
-        this.title = scene.add.text(0, -5, this.cardData.name, { fontSize: '13px', fontStyle:'bold' }).setOrigin(0.5);
-        this.shortDesc = scene.add.text(0, 40, descText, { fontSize: '10px', color: '#ccc', align: 'center', wordWrap: {width: 90} }).setOrigin(0.5);
+        this.title = scene.add.text(0, -5, this.computedData.name, { fontSize: '13px', fontStyle:'bold' }).setOrigin(0.5);
+        
+        // 2. ВЫВОДИМ СГЕНЕРИРОВАННЫЙ ТЕКСТ
+        // Он уже содержит правильные цифры (9 урона вместо 6) и новые эффекты
+        this.shortDesc = scene.add.text(0, 40, this.computedData.generatedDesc, { 
+            fontSize: '11px', color: '#ccc', align: 'center', wordWrap: {width: 90} 
+        }).setOrigin(0.5);
+        
         this.costCircle = scene.add.circle(-40, -60, 12, 0x00ffff);
         
+        // 3. ВЫВОДИМ СТОИМОСТЬ (ПРАВИЛЬНУЮ)
+        // Если стоимость изменилась, красим в зеленый
         let costColor = '#000';
-        if (this.cardData.cost < baseData.cost) costColor = '#008800'; 
+        if (this.computedData.cost < baseData.cost) costColor = '#008800'; 
         
-        this.costText = scene.add.text(-40, -60, this.cardData.cost, { fontSize: '16px', color: costColor, fontStyle: 'bold' }).setOrigin(0.5);
-        this.fullDesc = scene.add.text(0, 50, this.cardData.fullDesc, { fontSize: '9px', color: '#fff', align: 'center', wordWrap: { width: 90 } }).setOrigin(0.5).setVisible(false);
+        this.costText = scene.add.text(-40, -60, this.computedData.cost, { 
+            fontSize: '16px', color: costColor, fontStyle: 'bold' 
+        }).setOrigin(0.5);
+        
+        // Полное описание для зума (тоже берем сгенерированное)
+        this.fullDesc = scene.add.text(0, 50, this.computedData.generatedDesc, { 
+            fontSize: '14px', color: '#fff', align: 'center', wordWrap: { width: 150 } 
+        }).setOrigin(0.5).setVisible(false);
 
         this.add([this.bg, this.art, this.title, this.shortDesc, this.fullDesc, this.costCircle, this.costText]);
 
@@ -70,12 +72,9 @@ export class Card extends Phaser.GameObjects.Container {
 
     toggleMode(isZoomed) {
         this.isZoomed = isZoomed;
-        let strokeColor = 0x999999;
-        const baseData = CARDS_DB[this.cardInstance.id];
-        if (baseData.rarity === 'rare') strokeColor = 0x0088ff;
-        if (baseData.rarity === 'legendary') strokeColor = 0xffaa00;
-        if (this.cardInstance.enchants.length > 0) strokeColor = 0xff00ff;
-
+        // При зуме меняем рамку на голубую, при возврате - на родную
+        let strokeColor = this.bg.strokeColor; // Запоминаем текущий
+        
         if (isZoomed) { 
             this.shortDesc.setVisible(false); 
             this.fullDesc.setVisible(true); 
@@ -83,6 +82,14 @@ export class Card extends Phaser.GameObjects.Container {
         } else { 
             this.shortDesc.setVisible(true); 
             this.fullDesc.setVisible(false); 
+            
+            // Восстанавливаем цвет рамки
+            const baseData = CARDS_DB[this.cardInstance.id];
+            strokeColor = 0x999999;
+            if (baseData.rarity === 'rare') strokeColor = 0x0088ff;
+            if (baseData.rarity === 'legendary') strokeColor = 0xffaa00;
+            if (this.cardInstance.enchants.length > 0) strokeColor = 0xff00ff;
+            
             this.bg.setStrokeStyle(2, strokeColor); 
         }
     }
