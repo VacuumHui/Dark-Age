6e59b56bcf935835df16eff8e70efe0be389e777
VacuumHui <ragnarok16801050@gmail.com>
2026-01-19 08:24:14 +0300

    Update Card.js

--- src/prefabs/Card.js
+++ src/prefabs/Card.js
@@ -15,7 +15,7 @@ export class Card extends Phaser.GameObjects.Container {
         
         this.isZoomed = false;
         
-        const w = 120, h = 170;
+        const w = 180, h = 252;
         
         // Цвет рамки (редкость + зачарование)
         let strokeColor = 0x999999;
