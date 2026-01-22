309ea3a61405a6d59253d54349ef82ef3960b5b0
VacuumHui <ragnarok16801050@gmail.com>
2026-01-22 15:46:31 +0300

    Update EffectManager.js

--- src/managers/EffectManager.js
+++ src/managers/EffectManager.js
@@ -21,7 +21,7 @@ export class EffectManager {
         // Разлет искр
         const sparks = this.scene.add.particles(x, y, 'spark', {
             speed: { min: 230, max: 400 },
-            angle: { min: 20, max: 165},
+            angle: { min: 310, max: 30 },
             scale: { start: 1, end: 0 },
             tint: 0xffaa00, // Оранжевый
             lifespan: 500,
