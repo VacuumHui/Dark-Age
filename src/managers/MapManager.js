d6aa129f287b42a75565541e2ef3abaadff32fbd
VacuumHui <ragnarok16801050@gmail.com>
2026-01-26 14:23:55 +0300

    Update MapManager.js

--- src/managers/MapManager.js
+++ src/managers/MapManager.js
@@ -115,36 +115,48 @@ for (let x = 0; x < this.floors; x++) {
 
 globalThis.MapManager = MapManager;
   
-export class MapManager {
-    constructor() {
-        this.floors = 12;
-        this.nodeTypes = [
-            { type: 'battle', weight: 50 },
-            { type: 'event', weight: 25 },
-            { type: 'shop', weight: 10 },
-            { type: 'rest', weight: 15 }
-        ];
-    }
+if (!globalThis.MapManager) {
+    class MapManager {
+        constructor() {
+            this.floors = 12;
+            this.nodeTypes = [
+                { type: 'battle', weight: 50 },
+                { type: 'event', weight: 25 },
+                { type: 'shop', weight: 10 },
+                { type: 'rest', weight: 15 }
+            ];
+        }
 
-    generateMap() {
-        const map = [];
+        generateMap() {
+            const map = [];
 
-        // 1. Генерация этажей
-        for (let x = 0; x < this.floors; x++) {
-            const layer = [];
-           let count;
-            if (x === 0 || x === this.floors - 1) count = 1;
-            else if (x === this.floors - 2) count = 2;
-            else count = Math.floor(Math.random() * 3) + 3;
+            // 1. Генерация этажей
+            for (let x = 0; x < this.floors; x++) {
+                const layer = [];
+                let count;
+                if (x === 0 || x === this.floors - 1) count = 1;
+                else if (x === this.floors - 2) count = 2;
+                else count = Math.floor(Math.random() * 3) + 3;
 
-          const offsetY = (5 - count) / 2;
+                const offsetY = (5 - count) / 2;
 
-           for (let i = 0; i < count; i++) {
-             let type = 'battle';
-               if (x === 0) type = 'start';
-                else if (x === this.floors - 1) type = 'boss';
-               else if (x === this.floors - 2) type = 'rest';
-              else type = this.getRandomType();
+                for (let i = 0; i < count; i++) {
+                    let type = 'battle';
+                    if (x === 0) type = 'start';
+                    else if (x === this.floors - 1) type = 'boss';
+                    else if (x === this.floors - 2) type = 'rest';
+                    else type = this.getRandomType();
+
+                    layer.push({
+                        id: `${x}-${i}`,
+                        x,
+                        y: i + offsetY,
+                        type,
+                        connections: [],
+                        visible: x === 0,
+                        status: x === 0 ? 'available' : 'locked'
+                    });
+                }
 
                 layer.push({
                    id: `${x}-${i}`,
