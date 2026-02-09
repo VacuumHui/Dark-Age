5be6b97386c2baff7b59a7634e8cfc48ed1b8655
VacuumHui <ragnarok16801050@gmail.com>
2026-02-09 08:54:25 +0300

    Update enemies.js

--- src/data/enemies.js
+++ src/data/enemies.js
@@ -1,7 +1,10 @@
- export const ENEMIES_DB = {
+export const ENEMIES_DB = {
      // --- ОБЫЧНЫЕ ---
      "slime": {
-         name: "Ядовитый Слайм", hp: 40, color: 0x00aa44,
+         name: "Ядовитый Слайм", 
+         hp: 40, 
+         color: 0x00aa44,
+         cost: 1, // <--- НОВОЕ: Дешевый враг
          moves: [
             { name: "Тычок", chance: 0.45, actions: [{ type: "damage", value: 6 }] },
             { name: "Плевок", chance: 0.35, actions: [{ type: "damage", value: 3 }, { type: "apply_status", status: "poison", value: 2 }] },
@@ -9,7 +12,10 @@
          ]
      },
      "knight": {
-         name: "Рыцарь", hp: 80, color: 0x440088,
+         name: "Рыцарь", 
+         hp: 80, 
+         color: 0x440088,
+         cost: 3, // <--- НОВОЕ: Дорогой враг
          moves: [
            { name: "Удар", chance: 0.4, actions: [{ type: "damage", value: 10 }] },
           { name: "Клич", chance: 0.35, target: "self", actions: [{ type: "apply_status", status: "strength", value: 3 }, { type: "block", value: 5 }] },
@@ -21,19 +27,18 @@
      "boss_dragon": {
          name: "Древний Дракон", 
          hp: 250, 
-         color: 0xff4400, // Оранжевый
-         tier: "boss",    // МЕТКА БОССА
+         color: 0xff4400, 
+         cost: 10, // <--- НОВОЕ: Очень дорогой (занимает весь бюджет)
+         tier: "boss",    
          moves: [
-             // 1. Мощная атака
              { 
                  name: "Огненное дыхание", 
                 chance: 0.35, 
                  actions: [ 
                      { type: "damage", value: 15 },
-                     { type: "apply_status", status: "vulnerable", value: 2 } // Делает тебя уязвимым
+                     { type: "apply_status", status: "vulnerable", value: 2 } 
                  ] 
              },
-             // 2. Бафф
              { 
                  name: "Взлет",  
                chance: 0.35, 
@@ -43,11 +48,10 @@
                      { type: "apply_status", status: "strength", value: 2 }
                  ] 
              },
-             // 3. Мульти-атака (Когти)
              {
                  name: "Когти",
                  chance: 0.3,
-                 actions: [ { type: "damage", value: 8 } ] // Бьет слабее, но часто (можно прописать логику мультиудара позже)
+                 actions: [ { type: "damage", value: 8 } ] 
              }
          ]
      }
